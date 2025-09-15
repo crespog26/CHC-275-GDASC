8581229a05fd77fe6eadeed529fc89409e28da66
crespog26 <gabiscrispy@gmail.com>
2025-09-15 11:19:37 -0400

    Create calc.py

---
+++ Python stuff/calc.py
@@ -0,0 +1,29 @@
+print(f"Hi! welcome to the calc!!!!! (short for calculator)")
+
+a = input(f"Enter your first number ")
+a = int(a)
+b = input(f"Enter your second number ")
+b = int(b)
+sum = a+b
+print(sum)
+
+c = input(f"Enter your third number ")
+c = int(c)
+d = input(f"Enter your fourth number ")
+d = int(d)
+difference = c-d
+print(difference)
+
+e = input(f"Enter your fifth number ")
+e = int(e)
+f = input(f"Enter your sixth number ")
+f = int(f)
+product = e*f
+print(product)
+
+g = input(f"Enter your seventh number ")
+g = int(g)
+h = input(f"Enter your eighth number ")
+h = int(h)
+quotient = g/h
+print(quotient)
