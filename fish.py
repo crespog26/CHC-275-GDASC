b67dcd68657be9a04c775c783570152d24acb1bb
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-22 16:28:54 -0400

    Create fish.py

---
+++ fish.py
@@ -0,0 +1,17 @@
+
+
+
+
+print(f"Welcome to the docks! go fishing.")
+check = False
+while check == False:
+    print("Option 1: carnivorous.")
+    print("Option 2: saltwater.")
+    print("Option 3: community.")
+    option = input(f"These are your options. say 'i'm ready!' to begin fishing.")
+if input("i'm ready!"):
+    check = True
+elif:
+    print("take your time, but you need to be ready before you start fishing!")
+elif check == True:
+    print("placeholder")
