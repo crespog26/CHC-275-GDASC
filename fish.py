f0c294cb9ce1688d237658e8ecd151a64e5e2b88
WigginsM26 <WigginsM26@chcstudent.com>
2025-09-23 15:44:45 -0400

    Update fish.py

--- fish.py
+++ fish.py
@@ -3,15 +3,21 @@
 
 
 print(f"Welcome to the docks! go fishing.")
-check = False
-while check == False:
-    print("Option 1: carnivorous.")
-    print("Option 2: saltwater.")
-    print("Option 3: community.")
-    option = input(f"These are your options. say 'i'm ready!' to begin fishing.")
-if input("i'm ready!"):
-    check = True
-elif:
-    print("take your time, but you need to be ready before you start fishing!")
-elif check == True:
-    print("placeholder")
+print("Option 1: carnivorous.")
+print("Option 2: saltwater.")
+print("Option 3: community.")
+fishy1 = input(f"what kind of fish do you have? ")
+if fishy1 == ("carnivorous"):
+    fishy2 = input(f"do you already have it? ")
+    if fishy2 == ("yes"):
+        print("TOO BAD")
+    elif fishy2 == ("no"):
+        print("enjoy! :3")
+    else:
+        print("try again.")
+elif fishy1 == ("saltwater"):
+    print("you're a fancy fish parent! :O")
+elif fishy1 == ("community"):
+    print("you should get more than one!")
+else:
+    print("I dont think that's a type of fish.")
