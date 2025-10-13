309934cad74d74a6733c384828531b89c0b6c30b
WigginsM26 <WigginsM26@chcstudent.com>
2025-10-13 14:43:56 -0400

    Create parsing.py

---
+++ parsing.py
@@ -0,0 +1,52 @@
+"""
+strings and parsing strings 
+
+strings are ordered lissts of characters
+
+same ideas from lists also work for strings
+can use len for length
+
+sqaure bracket indicators
+only difference is way we add and remove 
+
+python knowledge bomb
+    reasson why we can use thing, lists and strings bot inherit from the name parent called an iterable << python trivia
+    
+inputs do not like typos
+    our desing pattern so far only works with very strict int parsing
+    
+python has a lot of built in functions that let us parse strings better
+ex.
+    .strip
+        removes bleeding and trailing white space
+        mystr = mystr.strip()
+    .lower
+        forces everything to be lowercase
+    .upper
+        forces everything to be uppercase
+    
+    
+isnumeric()
+    returns True if the stirngs its called on isa  number
+    
+/n separatres new text 
+    escape sequence
+    new line
+    
+parse my peom string and separate each line into tis own element of a list calles lines
+
+.split(lines) << retuyrbns a lkuist oidf strinfgs where each elenmet iss split by a neline
+
+final.io 
+    file inoput / output
+    done by parsing text from sude iof a .txt
+        textfile called accounts, had it have all anmes 
+    
+"""
+
+
+
+
+
+
+
