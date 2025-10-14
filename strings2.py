cfd715857aadb94339235dc4081a46010aa6ef1a
WigginsM26 <WigginsM26@chcstudent.com>
2025-10-14 13:44:00 -0400

    Create strings2.py

---
+++ strings2.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+"""
+can split mystr into a list of individual sst rings using
+    .split
+so you might get a data packet over a computer network that is delinated by a start and end sequence of bits
+
+
+"""
+
+
+
+mystr = ["Frank Teague Preston"]
+names = mystr.split()
+print(names)
+
