61edaf96508bb8033223f2a6208e22a8caaf3e38
WigginsM26 <WigginsM26@chcstudent.com>
2025-10-29 12:29:19 -0400

    Create exceptionhandling.py

---
+++ exceptionhandling.py
@@ -0,0 +1,50 @@
+
+"""
+imports
+- pygame
+
+blackjaaack!!
+project proposal
+
+string parsing?
+- modified our input and made sure it was valid for the operations we were tryna do
+had to wrap a lot of the checking for values inside if statements
+
+exclamation point?? >>!!!!!!!!!!!!!!
+we got 2 distinct error from the same code block? 
+exception handling 
+dealing with runtime errors in our program gracefully
+
+runtime errors
+- value error
+- division by 0
+- type error
+
+try:
+  code block
+except <exception>:
+  <if some runtime error happens, execute this block>
+finally:
+  <regardless if try or except succeds, run this block>
+
+we say a program has a runtime error has a runtime error
+---
+thingy not finished vv
+
+try:
+  x = int(input"enter num1: ")
+  y = int(input"enter num2: ")
+
+----
+
+except Exception as e:
+  #this captures all possible errors that are not value errors or 0 errors
+  print(e)
+  
+"""
+
+
+
+
+
+
