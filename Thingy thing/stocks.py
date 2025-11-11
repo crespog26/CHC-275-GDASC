c2bac7fd6ba37555f906bce8c71e01a92648c0da
crespog26 <gabiscrispy@gmail.com>
2025-11-11 11:31:51 -0500

    Update stocks.py

--- Thingy thing/stocks.py
+++ Thingy thing/stocks.py
@@ -1,13 +1,24 @@
 
 
+file = open("day.txt","r") #open("<filename>","<mode>") 
 
+buffer = file.readlines() 
+names = [] 
+grades = [] 
+print(buffer) 
+file.close() 
+for line in buffer: 
+    line = line.strip()
+    line = line.split(",") 
+    names.append(line[0])
+    grades.append(line[1])
+    
 
-file = open("day.txt", "r")
-buffer = file.readlines()
-file.close()
-print(buffer)
-print(buffer[0])
-print(buffer[1])
-print(buffer[2])
+try:
+    for i in range(len(grades)):
+        grades[i] = int(grades[i])
+except ValueError:
+    print("grade must be a number")
 
-
+print(names)
+print(grades)
