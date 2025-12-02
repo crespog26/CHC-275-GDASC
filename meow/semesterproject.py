d467749fb4136575565094b5920c2d877a4bf665
WigginsM26 <WigginsM26@chcstudent.com>
2025-12-02 14:44:08 -0500

    Update semesterproject.py

---
+++ meow/semesterproject.py
@@ -0,0 +1,28 @@
+
+#I used tutorials from yt channel ProgrammingKnowledge
+
+
+import pygame
+pygame.init()
+"""screen = pygame.display.set_mode((700,700))
+
+while True:
+    for event in pygame.event.get():"""
+        
+
+print(pygame.font.get_fonts())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
