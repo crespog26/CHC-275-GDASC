33cf46eba87d2fb51f4799c46a53ef12cdc2d828
WigginsM26 <WigginsM26@chcstudent.com>
2026-01-22 12:26:49 -0500

    Create functions.py sem2.py

---
+++ Python stuff/functions.py sem2.py
@@ -0,0 +1,27 @@
+"""
+code execution hsa owners and services 
+when we write a function or 
+
+"""
+
+#function example
+def foo(): #this is called trhe fyunction header 3 parts the fuicntion header.
+            #Def keyword. tjhos tell;s tjhe python interpeter that you are about to sp[eicfu a fuinction
+            # function name. this is the nam,e of the routine that yttou to use ex. foo ims te name 
+            #parameter list are tour inputs of your function , and they go insidfe the ()
+            
+    #started from line 63, this is the function body. function bodies need to indented
+    #when a function ends, you must unindent the codeblock
+    print("bar")
+
+foo()
+
+def add(x,y):
+    #1 def keyword 2 fucntion name 3 parameter list
+    #how parameters behave: they are just [p;laceholders for values that we can plug in later
+    print(x+y)
+
+add(6,7) 
+
+
+
